7014f75e4bebad9c9794ffa07b34927949bfb55d
Nii Nartey <ictkemba@gmail.com>
2024-08-21 21:35:03 +0000

    feat: add basic spring authentication

---
+++ src/main/java/com/advanced_lab/AdvancedLabApplication.java
@@ -0,0 +1,13 @@
+package com.advanced_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advanced_lab/AdvancedLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
