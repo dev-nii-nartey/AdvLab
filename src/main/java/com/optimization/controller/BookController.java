d436dafcff254a5667ff546b0ca1d23077359954
Nii Nartey <ictkemba@gmail.com>
2024-09-21 18:11:29 +0000

    add logging file path

--- src/main/java/com/optimization/controller/BookController.java
+++ src/main/java/com/optimization/controller/BookController.java
@@ -6,12 +6,13 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/books")
 @AllArgsConstructor
@@ -19,20 +20,33 @@ public class BookController {
     private IBookServiceImpl bookService;
 
     @GetMapping
-   @Cacheable("allBooksCache")
+//   @Cacheable("allBooksCache")
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     @GetMapping("/recommendations/{genre}")
-    @Cacheable(value = "recommendationsCache", key = "#genre")
+//    @Cacheable(value = "recommendationsCache", key = "#genre")
     public List<Book> getRecommendations(@PathVariable String genre) {
         return bookService.getRecommendations(genre);
     }
 
 
+    @GetMapping("/{id}")
+//    @Cacheable(value = "recommendationsCache", key = "#genre")
+    public Optional<Book> getBookByid(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+
+        if(book.isEmpty()) {
+            log.error("Book with id: "+id+" not found");
+            return Optional.empty();
+        }
+        return book;
+    }
+
+
     @PostMapping
-    @CacheEvict(value = {"allBooksCache", "recommendationsCache"}, allEntries = true)
+//    @CacheEvict(value = {"allBooksCache", "recommendationsCache"}, allEntries = true)
     public String addBook(@Valid @RequestBody Book book) {
        return bookService.addBook(book);
     }
